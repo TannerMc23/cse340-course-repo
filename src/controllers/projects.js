569b979f71447e81c94c649d8908a6daaa4eb013
tannermc23 <tmcmillan@byupathway.edu>
2026-05-20 20:30:28 -0600

    Completed learning activities and team activity

---
+++ src/controllers/projects.js
@@ -0,0 +1,18 @@
+import { getAllProjects, getUpcomingProjects, getProjectDetails } from '../models/projects.js';
+
+const NUMBER_OF_UPCOMING_PROJECTS = 5;
+
+const showProjectsPage = async (req, res) => {
+    const projects = await getUpcomingProjects(NUMBER_OF_UPCOMING_PROJECTS);
+    const title = 'Upcoming Service Projects';
+    res.render('projects', { title, projects });
+};
+
+const showProjectDetailsPage = async (req, res) => {
+    const projectId = req.params.id;
+    const project = await getProjectDetails(projectId);
+    const title = 'Service Project Details';
+    res.render('project', { title, project });
+};
+
+export { showProjectsPage, showProjectDetailsPage };
--- src/models/projects.js
+++ src/models/projects.js
@@ -18,4 +18,66 @@ const getAllProjects = async () => {
     return result.rows;
 };
 
-export { getAllProjects };
+const getProjectsByOrganizationId = async (organizationId) => {
+    const query = `
+        SELECT
+            project_id,
+            organization_id,
+            title,
+            description,
+            location,
+            date
+        FROM service_projects
+        WHERE organization_id = $1
+        ORDER BY date;
+    `;
+
+    const query_params = [organizationId];
+    const result = await db.query(query, query_params);
+    return result.rows;
+};
+
+const getUpcomingProjects = async (number_of_projects) => {
+    const query = `
+        SELECT
+            sp.project_id,
+            sp.title,
+            sp.description,
+            sp.date,
+            sp.location,
+            sp.organization_id,
+            o.name AS organization_name
+        FROM service_projects sp
+        JOIN organization o ON sp.organization_id = o.organization_id
+        WHERE sp.date >= CURRENT_DATE
+        ORDER BY sp.date ASC
+        LIMIT $1;
+    `;
+
+    const query_params = [number_of_projects];
+    const result = await db.query(query, query_params);
+    return result.rows;
+};
+
+const getProjectDetails = async (id) => {
+    const query = `
+        SELECT
+            sp.project_id,
+            sp.title,
+            sp.description,
+            sp.date,
+            sp.location,
+            sp.organization_id,
+            o.name AS organization_name
+        FROM service_projects sp
+        JOIN organization o ON sp.organization_id = o.organization_id
+        WHERE sp.project_id = $1;
+    `;
+
+    const query_params = [id];
+    const result = await db.query(query, query_params);
+    return result.rows.length > 0 ? result.rows[0] : null;
+};
+
+// Updated export
+export { getAllProjects, getProjectsByOrganizationId, getUpcomingProjects, getProjectDetails };
